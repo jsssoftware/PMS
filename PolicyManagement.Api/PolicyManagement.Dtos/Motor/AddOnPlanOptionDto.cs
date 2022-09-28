40c6c5fe2d6e92044d429ac6ad7a027b917555e1
Vivek <vivekparekh111@gmail.com>
2022-09-28 19:25:47 +0530

    first commit

---
+++ PolicyManagement.Api/PolicyManagement.Dtos/Motor/AddOnPlanOptionDto.cs
@@ -0,0 +1,10 @@
+﻿namespace PolicyManagement.Dtos.Motor
+{
+    public class AddOnPlanOptionDto
+    {
+        public int AddonPlanOptionId { get; set; }
+        public string AddonPlanOptionName { get; set; }
+        public string AddonPlanOptionDescripation { get; set; }
+        public bool IsPlanAvailable { get; set; }
+    }
+}
